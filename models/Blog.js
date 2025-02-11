ed30b92ce18c77a180ea9a07c7444317fa93dc22
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2025-02-11 14:11:41 -0500

    done with blogs

--- models/Blog.js
+++ models/Blog.js
@@ -31,6 +31,17 @@ const BlogSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+BlogSchema.virtual("formattedDate").get(function () {
+  return this.createdAt.toLocaleDateString("en-NG", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  });
+});
+
+// Ensure virtuals are included when converting to JSON
+BlogSchema.set("toJSON", { virtuals: true });
+BlogSchema.set("toObject", { virtuals: true });
 
 const Blog = mongoose.model("Blog", BlogSchema);
 module.exports = Blog;
